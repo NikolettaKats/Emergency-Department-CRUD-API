06d7dfcec8fd89618f3ad290a83515257fd00254
Nikoletta Katsivelou <154357527+NikolettaKats@users.noreply.github.com>
2026-06-23 17:36:42 +0300

    delete.py

--- delete.py
+++ delete.py
@@ -20,60 +20,6 @@ def delete_latest_admission(conn, ama):
 
 
 
-# def delete_patient(conn, patient_data):
-#     try:
-#         cursor = conn.cursor(dictionary = True) #to get results as dictionaries
-#         # insert_query_patient = """
-#         # SELECT * FROM admission WHERE ama = %s ORDER BY date_time DESC LIMIT 1;
-#         # """
-#         if 'ama' not in patient_data:
-#             return 'error'
-        
-#         delete_query_patient = """
-#         DELETE * FROM patient WHERE ama = %s;
-#         """
-        
-#         cursor.execute(delete_query_patient, patient_data)
-#         data = cursor.rowcount
-#         conn.commit()
-#         #
-#         # pprint.pprint(data)
-        
-#         cursor.close()
-        
-#     except Exception as e:
-#         print(e)
-#         return None
 
-#     return data
-
-
-
-# def delete_admission(conn, admission_data):
-#     try:
-#         cursor = conn.cursor(dictionary = True) #to get results as dictionaries
-#         # insert_query_patient = """
-#         # SELECT * FROM admission WHERE ama = %s ORDER BY date_time DESC LIMIT 1;
-#         # """
-#         if 'ama' not in admission_data:
-#             return 'error'
-        
-#         delete_query_admission = """
-#         DELETE * FROM admission WHERE ama = %s; 
-#         """
-
-#         cursor.execute(delete_query_admission, admission_data)
-#         data = cursor.rowcount
-#         conn.commit()
-#         #
-#         # pprint.pprint(data)
-        
-#         cursor.close()
-        
-#     except Exception as e:
-#         print(e)
-#         return None
-
-#     return data
 
 
